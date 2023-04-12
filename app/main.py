e9f7de53bcf029960eadc8a0cb6c92ede0e9d30e
WenhanJiang <wjia83@student.monash.edu>
2023-04-12 10:34:07 +0000

    try

--- app/main.py
+++ app/main.py
@@ -1,8 +1,12 @@
 from fastapi import FastAPI
-
+from .import sqrt
 
 app=FastAPI()
 
 @app.get("/")
 def index():
     return('hello data')
+@app.post("/{number}")
+def calculate(number:int):
+    return sqrt(number)
+
